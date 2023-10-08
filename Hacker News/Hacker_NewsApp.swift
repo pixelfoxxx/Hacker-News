182695cb9c11119d01cc5467cb6d182a9b548ccb
yurii kleimenov <yurakleimenovv@gmail.com>
2023-10-08 15:01:49 +0300

    Initial Commit

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Юрий Клеймёнов on 08/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
